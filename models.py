c896f50c46eef921bdcaec256e903ec6f1f388b2
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-05-07 17:41:01 -0400

    connected to model

---
+++ models.py
@@ -0,0 +1,20 @@
+from peewee import *
+from flask_login import UserMixin
+
+DATABASE = SqliteDatabase('data.sqlite')
+
+class User(UserMixin, Model):
+  username=CharField(unique=True)
+  email=CharField(unique=True)
+  password=CharField()
+
+  class Meta:
+    database = DATABASE
+
+def initialize():
+  DATABASE.connect()
+
+  DATABASE.create_tables([User], safe=True)
+  print('connected to database')
+
+  DATABASE.close()
